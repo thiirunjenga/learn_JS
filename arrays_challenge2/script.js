c88d44941de7d83c168bdc27680ffee1a2575d63
thiirunjenga <thiiru.n@gmail.com>
2023-08-03 10:29:47 +0300

    updated solution

--- arrays_challenge2/script.js
+++ arrays_challenge2/script.js
@@ -1,7 +1,12 @@
 const arr1 = [1, 2, 3, 4, 5];
 const arr2 = [5, 6, 7, 8, 9];
 
-arr1.pop();
+//first option
+arr1.pop();    //remove element on last index
+
+
+//second option
+//arr2.shift();  //remove element on first index
 let newArr = [...arr1, ...arr2];
 
 
